612cc82e0b9b5a8624c61ea881dab22618dd438d
[Konstantin Kolesnikov] <[kk888t@gmail.com]>
2021-03-24 09:02:53 -0400

    Add answer to quastion #2

--- sql_ex.sql
+++ sql_ex.sql
@@ -1,1 +1,10 @@
-select * from sda
+-- 1. Write a query to display the name, product line, and buy price of all products. 
+-- The output columns should display as “Name”, “Product Line”, and “Buy Price”. 
+-- The output should display the most expensive items first.
+
+select productName as Name, productLine as ProductLine, buyPrice as BuyPrice from products order by buyPrice desc;
+
+-- 2. Write a query to display the first name, last name, and city for all customers from Germany. 
+-- Columns should display as “First Name”, “Last Name”, and “City”. 
+-- The output should be sorted by the customer’s last name (ascending).
+
