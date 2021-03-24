b740f7ff816b9a374062d1f56618a89516b6e586
[Konstantin Kolesnikov] <[kk888t@gmail.com]>
2021-03-24 09:14:12 -0400

    Add answer for quastion #2(corrected)

--- sql_ex.sql
+++ sql_ex.sql
@@ -2,7 +2,7 @@
 -- The output columns should display as “Name”, “Product Line”, and “Buy Price”. 
 -- The output should display the most expensive items first.
 
-select productName as Name, productLine as ProductLine, buyPrice as BuyPrice from products order by buyPrice desc;
+select productName as Name, productLine as `Product Line`, buyPrice as `Buy Price` from products order by buyPrice desc;
 
 -- 2. Write a query to display the first name, last name, and city for all customers from Germany. 
 -- Columns should display as “First Name”, “Last Name”, and “City”. 
