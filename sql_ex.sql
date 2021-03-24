3f42f81c12a0d550004e9fc101f30c2b92f22436
[Konstantin Kolesnikov] <[kk888t@gmail.com]>
2021-03-24 09:24:22 -0400

    Add answer for quastion #3

--- sql_ex.sql
+++ sql_ex.sql
@@ -8,3 +8,12 @@ select productName as Name, productLine as `Product Line`, buyPrice as `Buy Pric
 -- Columns should display as “First Name”, “Last Name”, and “City”. 
 -- The output should be sorted by the customer’s last name (ascending).
 
+select contactFirstName as `First Name`, contactLastName as `Last Name`, city as `City` from customers where country='Germany';
+
+-- 3. Write a query to display each of the unique values of the status field in the orders table. 
+-- The output should be sorted alphabetically increasing. Hint: the output should show exactly 6 rows.
+
+select distinct status as `Status` from orders order by status;
+
+-- 4. Select all fields from the payments table for payments made on or after January 1, 2005. 
+-- Output should be sorted by increasing payment date.
